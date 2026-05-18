01665634ff7d10cc69e6089657e5602e2c552e4a
Chad Beaudin <chad@thebeaudins.com>
2026-05-17 20:50:00 -0700

    fix: escape XML special characters in GPX route name
    Closes #19

--- lib/gpx.ts
+++ lib/gpx.ts
@@ -1,14 +1,24 @@
+function escapeXml(str: string): string {
+    return str
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&apos;');
+}
+
 export function buildGpxCourse(
     route: [number, number, number?, number?][],
     name = 'StreetSweep Course'
 ): string {
+    const safeName = escapeXml(name);
     return `<?xml version="1.0" encoding="UTF-8"?>
 <gpx version="1.1" creator="StreetSweep" xmlns="http://www.topografix.com/GPX/1/1">
   <metadata>
-    <name>${name}</name>
+    <name>${safeName}</name>
   </metadata>
   <trk>
-    <name>${name}</name>
+    <name>${safeName}</name>
     <trkseg>
 ${route.map(pt => `      <trkpt lat="${pt[1]}" lon="${pt[0]}">${pt[2] !== undefined ? `\n        <ele>${pt[2]}</ele>` : ''}
       </trkpt>`).join('\n')}
